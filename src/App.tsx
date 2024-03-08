0121d16ee6b4b571b2f55b57b774969325c8d5fd
Kalinga <kalingakhatua@gmail.com>
2024-03-08 16:26:40 +0530

    minor commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import { useState } from 'react'
-
-import viteLogo from '/vite.svg'
-import './App.css'
-import ServicePage from './components/ServicePage'
+import "./App.css";
+import ServicePage from "./components/ServicePage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      
-      <ServicePage/>
-      </>
-  )
+	return (
+		<>
+			<ServicePage />
+		</>
+	);
 }
 
-export default App
+export default App;
