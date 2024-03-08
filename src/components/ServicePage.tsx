aef592630704a6d38189497b470f8ef326104ef7
ankitasahu2911 <145644574+ankitasahu2911@users.noreply.github.com>
2024-03-08 14:16:42 +0530

    new3

--- src/components/ServicePage.tsx
+++ src/components/ServicePage.tsx
@@ -1,6 +1,39 @@
+import { Accordion, AccordionSummary, AccordionDetails, AccordionActions, Button, Typography } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 
 export default function ServicePage() {
   return (
-	<div>ServicePage</div>
+    <div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<AddIcon/>}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          <Typography>Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<AddIcon />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+        >
+          <Typography>Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
   )
 }
