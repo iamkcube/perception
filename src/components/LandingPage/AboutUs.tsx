f535e674a4aebc45daf60012e7ca8447e71faf8d
ankitasahu2911 <145644574+ankitasahu2911@users.noreply.github.com>
2024-03-09 20:52:53 +0530

    positionchange

--- src/components/LandingPage/AboutUs.tsx
+++ src/components/LandingPage/AboutUs.tsx
@@ -10,18 +10,18 @@ export default function AboutUs() {
 			}}
 		>
 			<div style={{backgroundColor:'black', flex:'1'}}>
-              <h6 style={{position: 'relative' ,left:'2rem'}}>[01]</h6>
-			  <h1 style={{fontSize:'2rem', textAlign:'left', position: 'relative' ,left:'2rem'}}>About us</h1>
+              <h6>[01]</h6>
+			  <h1 style={{fontSize:'2rem', textAlign:'left'}}>About us</h1>
 
 			</div>
-			<div style={{flex:'1',backgroundColor:'blue'}}>
+			<div style={{flex:'1',backgroundColor:'black'}}>
 				<ul>
 					<li>
 						[who we are]</li>
 				</ul>
-				<img src="https://fastly.picsum.photos/id/516/1080/720.jpg?hmac=_lNpyxgHTiAMdJncpoWeYSaN69YiW6exL7gwTTUHwYI" style={{height:'720', width:'1080' ,padding:'2rem'}} alt="" />
-				<h2 style={{position: 'relative' ,left:'2rem'}}>Natech</h2>
-				<p style={{position: 'relative' ,left:'2rem'}}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, facere? Excepturi temporibus dicta vero dolores maiores similique facilis quasi, modi veritatis! Voluptate sed, sequi cupiditate nulla odit cumque soluta quos ipsum rerum nobis quae temporibus officiis?</p>
+				<img src="https://fastly.picsum.photos/id/516/1080/720.jpg?hmac=_lNpyxgHTiAMdJncpoWeYSaN69YiW6exL7gwTTUHwYI" style={{paddingRight:'2rem'}} alt="" />
+				<h2>Natech</h2>
+				<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, facere? Excepturi temporibus dicta vero dolores maiores similique facilis quasi, modi veritatis! Voluptate sed, sequi cupiditate nulla odit cumque soluta quos ipsum rerum nobis quae temporibus officiis?</p>
             <CustomButton onClick={() => {}}>Read More</CustomButton>
 			</div>
 			
--- src/components/LandingPage/MeetOurTeam.tsx
+++ src/components/LandingPage/MeetOurTeam.tsx
@@ -2,14 +2,52 @@
 
 export default function MeetOurTeam() {
   return (
+    <div>
+      <h2 style={{textAlign:'center', fontSize:'3rem', backgroundColor:'black'}}>Meet our team </h2>
+    
     <div
     style={{
-      display: "flex", justifyContent:"space-between", margin:'4rem'
+      //  display: 'grid',
+      
+      // gridTemplateColumns: '4fr 4fr 4fr',
+      // grid-template-columns: repeat(4,auto);
+      // grid-gap: 2rem;
+      display:'flex', flexWrap:'wrap',
+       justifyContent:"center", gap:'2rem',backgroundColor:'black',
+      
+      }}>
       
       
-    }}
-  >
-      
-    </div>
+
+      <div style={{backgroundColor:'blue', margin:'1rem', padding:'1rem', width:'20%'}}> 
+      <img style={{height:'5rem'}} src="https://fastly.picsum.photos/id/516/1080/720.jpg?hmac=_lNpyxgHTiAMdJncpoWeYSaN69YiW6exL7gwTTUHwYI" alt="" />
+      <h3>Ankita Sahu</h3>
+      </div>
+      <div style={{backgroundColor:'blue' , margin:'1rem' , padding:'1rem', width:'20%'}}> 
+      <img style={{height:'5rem'}} src="https://fastly.picsum.photos/id/516/1080/720.jpg?hmac=_lNpyxgHTiAMdJncpoWeYSaN69YiW6exL7gwTTUHwYI" alt="" />
+      <h3>Ankita Sahu</h3>
+      </div>
+      <div style={{backgroundColor:'blue' , margin:'1rem' , padding:'1rem', width:'20%'}}> 
+      <img style={{height:'5rem' }} src="https://fastly.picsum.photos/id/516/1080/720.jpg?hmac=_lNpyxgHTiAMdJncpoWeYSaN69YiW6exL7gwTTUHwYI" alt="" />
+      <h3>Ankita Sahu</h3>
+      </div>
+      <div style={{backgroundColor:'blue' , margin:'1rem' , padding:'1rem', width:'20%'}}> 
+      <img style={{height:'5rem'}} src="https://fastly.picsum.photos/id/516/1080/720.jpg?hmac=_lNpyxgHTiAMdJncpoWeYSaN69YiW6exL7gwTTUHwYI" alt="" />
+      <h3>Ankita Sahu</h3>
+      </div>
+      <div style={{backgroundColor:'blue' , margin:'1rem' , padding:'1rem', width:'20%'}}> 
+      <img style={{height:'5rem'}} src="https://fastly.picsum.photos/id/516/1080/720.jpg?hmac=_lNpyxgHTiAMdJncpoWeYSaN69YiW6exL7gwTTUHwYI" alt="" />
+      <h3>Ankita Sahu</h3>
+      </div>
+      <div style={{backgroundColor:'blue' , margin:'1rem' , padding:'1rem', width:'20%'}}> 
+      <img style={{height:'5rem'}} src="https://fastly.picsum.photos/id/516/1080/720.jpg?hmac=_lNpyxgHTiAMdJncpoWeYSaN69YiW6exL7gwTTUHwYI" alt="" />
+      <h3>Ankita Sahu</h3>
+      </div>
+      <div style={{backgroundColor:'blue' , margin:'1rem' , padding:'1rem', width:'20%'}}> 
+      <img style={{height:'5rem'}} src="https://fastly.picsum.photos/id/516/1080/720.jpg?hmac=_lNpyxgHTiAMdJncpoWeYSaN69YiW6exL7gwTTUHwYI" alt="" />
+      <h3>Ankita Sahu</h3>
+      </div>
+</div>
+</div>
   )
 }
