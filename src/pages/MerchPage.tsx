17a836720bbd6e82caf13dbb35766eb2cd50080b
Kalinga <kalingakhatua@gmail.com>
2024-03-13 15:07:12 +0530

    merch page started

--- src/pages/MerchPage.tsx
+++ src/pages/MerchPage.tsx
@@ -1,9 +1,15 @@
 import { Box, Typography } from "@mui/material";
 
 export default function MerchPage() {
-  return (
-	<Box>
-    <Typography variant="h1">Perception Merchandise</Typography>
-  </Box>
-  )
+	return (
+		<Box
+			sx={{
+				display: "grid",
+				minHeight: "100svh",
+				placeItems: "center",
+			}}
+		>
+			<Typography variant="h1">Perception Merchandise</Typography>
+		</Box>
+	);
 }
